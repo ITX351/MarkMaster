98d56d4f9dd749ca28547bd61da8ef1bef36ea20
ITX351 <itx351@126.com>
2025-01-25 16:56:12 +0800

    解决DPI缩放导致的问题

--- MarkMaster/Program.cs
+++ MarkMaster/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace MarkMaster
 {
     internal static class Program
@@ -5,12 +7,22 @@ namespace MarkMaster
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDpiAwarenessContext(int dpiFlag);
+        private const int DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 = -4;
+
         [STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            // 禁用系统缩放
+            SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
             ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
         }
     }
